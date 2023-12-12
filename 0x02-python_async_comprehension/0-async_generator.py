c4e4bc5dd751c328cbf01be0af37a24824e50f33
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-12-12 21:20:53 +0100

    done with task 1 and 2

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Async and generators."""
 
-from typing import Generator
+from typing import AsyncGenerator
 import random
 import asyncio
 
 
-async def async_generator() -> Generator[int]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Wait for 1 second and yield a random number."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 11)
+        yield random.uniform(0, 10)
