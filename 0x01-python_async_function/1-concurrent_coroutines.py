fa7a46e38ec15da8b031251f431ffd39063924c5
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-12-17 09:51:51 +0100

    using asyncio.gather in task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,14 +4,11 @@ Running multiple routines
 """
 
 from typing import List
+import asyncio
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Calls wait_random @n times and passes @max_delay to it."""
-    res = []
-    for i in range(n):
-        sec = await wait_random(max_delay)
-        res.append(sec)
-    return res
+    return await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
