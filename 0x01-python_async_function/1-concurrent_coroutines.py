85008ea938fe44e6b77f5b8bb06407c6eb6359e8
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-12-17 08:44:16 +0100

    added type annotations for task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,11 @@
 Running multiple routines
 """
 
-import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Calls wait_random @n times and passes @max_delay to it."""
     res = []
     for i in range(n):
