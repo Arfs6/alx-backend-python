da6daa2200389daa238de12c563971aa87ab0bf5
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-12-17 09:58:47 +0100

    fixed task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Creates an asyncio task."""
 
-# import asyncio
+import asyncio
 from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
@@ -9,8 +9,4 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Calls wait_random @n times and passes @max_delay to it."""
-    res = []
-    for _ in range(n):
-        sec = await task_wait_random(max_delay)
-        res.append(sec)
-    return res
+    return await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
